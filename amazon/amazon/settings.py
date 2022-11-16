90a212601abce2491d7c6aa508170b813dd1198a
Yasir Azeem <yasirazeem77@gmail.com>
2022-11-17 02:57:10 +0500

    complete script

--- amazon/amazon/settings.py
+++ amazon/amazon/settings.py
@@ -14,10 +14,12 @@ NEWSPIDER_MODULE = 'amazon.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'amazon (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
+
+LOG_LEVEL = "INFO"
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- amazon/amazon/spiders/amz_cat.py
+++ amazon/amazon/spiders/amz_cat.py
@@ -1,10 +1,38 @@
 import scrapy
-
+from bs4 import BeautifulSoup
 
 class AmzCatSpider(scrapy.Spider):
     name = 'amz_cat'
     allowed_domains = ['amazon.com']
-    start_urls = ['http://amazon.com/']
+    start_urls = ['https://www.amazon.com/Best-Sellers-Welcome/zgbs/']
 
     def parse(self, response):
-        pass
+        soup = BeautifulSoup(response.body,'lxml')
+        
+        urls = soup.find_all('div',{'role':'treeitem'})
+        urls = [x.find('a') for x in urls if "/Best-Sellers" in str(x)]
+        
+        for x in urls:
+            url = "https://www.amazon.com"+x.get('href')
+            item = {}
+            item['category'] = x.text
+            yield item
+            yield scrapy.Request(url = url, callback=self.parse_template, meta={'tree': x.text, 'last_i':x.text})
+        
+
+    def parse_template(self, response):
+        soup = BeautifulSoup(response.body,'lxml')
+        last_i = response.meta['last_i']
+        sub_cats = soup.find_all('div',{'role':'treeitem'})
+        check = False
+        for sc in sub_cats:
+            if not check:
+                if sc.text == last_i:
+                    check = True
+                continue
+            item = {} 
+            item['category'] = response.meta['tree'] + ">"+sc.text.strip()
+            yield item
+            url = "https://www.amazon.com" + sc.find('a').get('href')
+            yield scrapy.Request(url = url, callback=self.parse_template, meta={'tree':item['category'], 'last_i':sc.text})
+        
